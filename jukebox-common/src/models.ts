dba3ce14f0403b2d252a228e370952554de43073
Jason B <jason@browne.me.uk>
2018-08-06 16:39:14 +0100

    Remove ‘requested by’ and add length to track info

--- jukebox-common/src/models.ts
+++ jukebox-common/src/models.ts
@@ -1,14 +1,10 @@
-export interface User {
-  name: string;
-}
-
 export interface TrackData {
   title: string;
   album: string;
   artist: string;
   songId: string;
+  lengthMs: number;
   requestedBy?: string; // Intent to deprecate
-  trackLength?: any; // Intent to make mandatory
 }
 
 export interface ClientData {
