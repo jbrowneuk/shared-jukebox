14c0b60babaa191c78be44bdc9cc3a0a0a1974c5
Jason B <jason@browne.me.uk>
2018-08-06 14:09:24 +0100

    Cover informational message display in app component

--- web-client/src/app/app.component.spec.ts
+++ web-client/src/app/app.component.spec.ts
@@ -1,22 +1,25 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-import { IMock, Mock } from 'typemoq';
-import { of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { IMock, Mock, It } from 'typemoq';
+import { BehaviorSubject } from 'rxjs';
 
 import { AppComponent } from './app.component';
 import { SocketService } from './socket.service';
+import { ServerEvents } from '../../node_modules/jukebox-common';
 
 describe('AppComponent', () => {
   let mockSocketService: IMock<SocketService>;
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let mockConnectionSubject: BehaviorSubject<boolean>;
 
   beforeEach(async(() => {
+    mockConnectionSubject = new BehaviorSubject<boolean>(true);
+
     mockSocketService = Mock.ofType<SocketService>();
     mockSocketService
       .setup(m => m.connection$)
-      .returns(() => of(true).pipe(delay(4)));
+      .returns(() => mockConnectionSubject.asObservable());
 
     TestBed.configureTestingModule({
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
@@ -34,5 +37,37 @@ describe('AppComponent', () => {
 
   it('should create the app', async(() => {
     expect(component).toBeTruthy();
+  }));
+
+  it('should not report outdated state if socket has not sent outdated message', async(() => {
+    mockSocketService.setup(m => m.subscribe(It.isValue(ServerEvents.ClientOutdated), It.isAny()));
+    component.ngOnInit();
+
+    setTimeout(() => {
+      expect(component.requiresUpdate).toBe(false);
+    });
+  }));
+
+  it('should report outdated state if socket has sent outdated message', async(() => {
+    let callbackFunc: Function = null;
+    mockSocketService.setup(m => m.subscribe(It.isValue(ServerEvents.ClientOutdated), It.isAny()))
+      .callback((_, func: Function) => callbackFunc = func);
+
+    component.ngOnInit();
+    callbackFunc.call(component);
+
+    setTimeout(() => {
+      expect(component.requiresUpdate).toBe(true);
+    });
+  }));
+
+  it('should display disconnected message when disconnected', async(() => {
+    mockConnectionSubject.next(false);
+
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const relatedElement = document.querySelector('[data-identifier=disconnected-message]');
+      expect(relatedElement).toBeTruthy();
+    });
   }));
 });
