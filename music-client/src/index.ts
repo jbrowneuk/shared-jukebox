f7556653ae3f4955c539719a41235b2bc638f135
Jason B <jaysunbee@gmail.com>
2018-08-09 20:23:34 +0100

    Implement configuration file for music client

--- music-client/src/index.ts
+++ music-client/src/index.ts
@@ -1,13 +1,15 @@
+import * as fs from 'fs';
+
 import { SocketClient } from './socket-client';
 import { MopidyClient, MopidyEvents } from './mopidy-client';
 
-const mopidyUrl = 'ws://talon.local:6680/mopidy/ws/';
-const socketUrl = 'http://localhost:8080';
+const raw = fs.readFileSync('./config.json', { encoding: 'utf8' });
+const settings = JSON.parse(raw);
 
-const mopidyClient = new MopidyClient(mopidyUrl);
+const mopidyClient = new MopidyClient(settings.mopidyUrl);
 
 function handleMopidyOnline(): void {
-  const socketClient = new SocketClient(socketUrl, mopidyClient);
+  const socketClient = new SocketClient(settings.serverUrl, mopidyClient);
   socketClient.initialize();
 }
 
